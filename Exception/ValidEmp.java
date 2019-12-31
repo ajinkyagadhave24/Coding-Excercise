7b4da9ddfb7a41ba59315bd8a4a2d3436cd3bfa5
ajinkyagadhave24 <59189165+ajinkyagadhave24@users.noreply.github.com>
2019-12-31 10:07:11 +0530

    Update ValidEmp.java

--- Exception/ValidEmp.java
+++ Exception/ValidEmp.java
@@ -9,6 +9,18 @@ public class ValidEmp {
 		this.fname = fname;
 		this.lname = lname;
 	}
+	public String getFname() {
+		return fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
 
 	public void validate() throws NameEmpException
 	{
@@ -17,8 +29,19 @@ public class ValidEmp {
 		else
 			System.out.println("Employee Name is valid");
 	}
+	@Override
+	public String toString() {
+		return "Employee [fname=" + fname + ", lname=" + lname + "]";
+	}
 	public static void main(String[] args) {
-		ValidEmp v= new ValidEmp("Rohan","Shelar");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter FULL NAME: ");
+		String fname=sc.next();
+		String lname=sc.next();
+		
+		ValidEmp v= new ValidEmp(fname,lname);
+		System.out.println("Entered name is:-");
+		System.out.println(v);
 		try
 		{
 		v.validate();
